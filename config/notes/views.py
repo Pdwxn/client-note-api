345994e3ccc19b08f8e6883db1fd237d45ede7e3
pdwxn <pdwxn@protonmail.com>
2026-04-15 15:07:22 -0400

    HotFix Import IsAuthenticated

--- config/notes/views.py
+++ config/notes/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 
