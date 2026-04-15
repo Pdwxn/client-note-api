69f430b636fb71a4be47c52918714ba20a72c896
pdwxn <pdwxn@protonmail.com>
2026-04-15 15:06:38 -0400

    Add permission classes to NoteViewSet for ownership validation

--- config/notes/views.py
+++ config/notes/views.py
@@ -5,12 +5,14 @@ from rest_framework.filters import OrderingFilter
 from .models import Note
 from .serializers import NoteSerializer
 from .filters import NoteFilter
+from .permissions import IsOwner
 
 class NoteViewSet(ModelViewSet):
     serializer_class = NoteSerializer
     filterset_class = NoteFilter
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     ordering_fields = ['created_at', 'title']
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def get_queryset(self):
         return Note.objects.filter(user=self.request.user)
