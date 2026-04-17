4999e26245a42f54e393eaf445ca4ce9d003c005
pdwxn <pdwxn@protonmail.com>
2026-04-17 17:59:27 -0400

    Update JWT settings to include refresh token lifetime and modify URL routing for token management endpoints.

--- config/config/urls.py
+++ config/config/urls.py
@@ -22,10 +22,8 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
 
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('api/', include(router.urls)),
-
-    path('api/auth/', include('rest_framework.urls')),
-
-    path('api/auth/login/', TokenObtainPairView.as_view()),
-    path('api/auth/refresh/', TokenRefreshView.as_view()),
 ]
