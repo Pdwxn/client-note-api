c7c290368c6892f46b204e381ef31ca79ab97c0e
pdwxn <pdwxn@protonmail.com>
2026-04-21 17:23:03 -0400

    Add 'me' endpoint for authenticated user details and update permissions in RegisterView. Enhance URL routing to include the new endpoint.

--- config/users/views.py
+++ config/users/views.py
@@ -1,5 +1,7 @@
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 
@@ -8,3 +10,12 @@ class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def me(request):
+    user = request.user
+    return Response({
+        "id": user.id,
+        "username": user.username,
+    })
