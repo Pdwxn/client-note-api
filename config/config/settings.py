4b07934ccb82c7999521840e3082baa77acf6f63
pdwxn <pdwxn@protonmail.com>
2026-04-28 00:08:38 -0400

    fix cors

--- config/config/settings.py
+++ config/config/settings.py
@@ -94,4 +94,5 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "").split(",")
+CORS_ALLOW_CREDENTIALS = True
